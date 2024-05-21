e70d5a35553dc24c8eda94c402e3878c345b2500
DiegoMerinoMas <u202111473@upc.edu.pe>
2024-05-21 16:12:43 -0500

    chore: initial commit

---
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/ChaquitacllaPlatformOpenApplication.java
@@ -0,0 +1,13 @@
+package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaquitacllaPlatformOpenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaquitacllaPlatformOpenApplication.class, args);
+	}
+
+}
