c8954f4ebaff6fbd55bcda23495d9057e789bec6
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-28 20:09:40 -0500

    feat(forum): corrected commands.

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/domain/model/commands/CreateAnswerCommand.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/domain/model/commands/CreateAnswerCommand.java
@@ -2,7 +2,6 @@ package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.doma
 
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.domain.model.aggregates.Question;
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.domain.model.valueobjects.UserId;
-import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.aggregates.User;
 
 public record CreateAnswerCommand(UserId userId, Question question, String answer) {
 }
