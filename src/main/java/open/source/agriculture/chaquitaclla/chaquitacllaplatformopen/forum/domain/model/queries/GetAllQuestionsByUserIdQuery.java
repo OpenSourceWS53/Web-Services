8564e6be35d2a34c126ffc5fdde1c646395f361a
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-05-30 17:59:14 -0500

    feat(forum): corrected question query.

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/domain/model/queries/GetAllQuestionsByUserIdQuery.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/domain/model/queries/GetAllQuestionsByUserIdQuery.java
@@ -1,4 +1,6 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.domain.model.queries;
 
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.domain.model.valueobjects.UserId;
+
 public record GetAllQuestionsByUserIdQuery(Long userId) {
 }
