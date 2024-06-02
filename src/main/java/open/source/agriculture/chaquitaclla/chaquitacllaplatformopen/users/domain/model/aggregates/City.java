85e38bebe2c3d192892172648599b98541f029aa
Godofredo <u202120772@upc.edu.pe>
2024-06-02 10:56:33 -0500

    City
    wyy

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/City.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/City.java
@@ -1,4 +1,20 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.aggregates;
 
-public class City {
+import jakarta.persistence.*;
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.shared.domain.model.entities.AuditableModel;
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.entities.Country;
+
+
+@Entity
+public class City extends AuditableModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    //@ManyToOne
+   // @JoinColumn(name = "country_id", nullable = false)
+   // private Country country;
+    
+
 }
