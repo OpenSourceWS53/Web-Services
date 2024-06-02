2101222021d68f77a0aa8896421b5ee0bdfcd8dd
Godofredo <u202120772@upc.edu.pe>
2024-06-02 18:28:51 -0500

    Tablas Creadas
    Al parecer User es una palabra ya reservada

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/User.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/User.java
@@ -2,72 +2,69 @@ package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.doma
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.Getter;
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.valueobjects.NameUserRecord;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.AbstractAggregateRoot;
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.valueobjects.EmailDirection;
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.valueobjects.Password;
-
 import java.util.Date;
 
 @Entity
+@Table(name = "\"user\"")
 public class User extends AbstractAggregateRoot<User> {
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Embedded
     private NameUserRecord name;
 
-
-
     @Embedded
     @Email
-    @AttributeOverride(name = "emialDirection",column = @Column(name = "email"))
+    @AttributeOverride(name = "emialDirection", column = @Column(name = "email"))
     private EmailDirection email;
 
     @Embedded
-    @AttributeOverride(name = "password",column = @Column(name = "password"))
+    @AttributeOverride(name = "password", column = @Column(name = "password"))
     private Password password;
-
 
     @ManyToOne
     private City city;
 
-
     @ManyToOne
     private Subscription subscription;
+
     @CreatedDate
-    private Date star_date;
+    private Date start_date;
+
     @CreatedDate
     private Date end_date;
 
+    public User() {}
 
-    public User() {
-    }
-
-    public User(String firstName,String  lastName, String email, String password) {
-        this.name = new NameUserRecord(firstName,lastName);
+    public User(String firstName, String lastName, String email, String password) {
+        this.name = new NameUserRecord(firstName, lastName);
         this.email = new EmailDirection(email);
         this.password = new Password(password);
     }
-    
+
     public Long getId() {
         return id;
     }
+
     public void updateName(String firstName, String lastName) {
         this.name = new NameUserRecord(firstName, lastName);
     }
-    public String getFullName(){
+
+    public String getFullName() {
         return this.name.fullName();
     }
 
-    public Date getStar_date() {
-        return star_date;
+    public Date getStart_date() {
+        return start_date;
     }
+
     public Date getEnd_date() {
         return end_date;
     }
-
 }
