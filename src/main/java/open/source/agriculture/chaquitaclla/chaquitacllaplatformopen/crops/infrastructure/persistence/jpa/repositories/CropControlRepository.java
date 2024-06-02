db6c100885cb22d864c8e2753e5d80066ece216a
diegocantoralp <u20201f568@upc.edu.pe>
2024-06-01 20:11:07 -0500

    feat(crops): created repositories for crops

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/crops/infrastructure/persistence/jpa/repositories/CropControlRepository.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/crops/infrastructure/persistence/jpa/repositories/CropControlRepository.java
@@ -1,10 +1,16 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.infrastructure.persistence.jpa.repositories;
 
 import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.domain.model.entities.CropControl;
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.domain.model.entities.Care;
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.domain.model.entities.Disease;
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.domain.model.entities.Pest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CropControlRepository extends JpaRepository<CropControl, Long> {
     List<CropControl> findByCropId(Long id);
+    List<CropControl> findByCare(Care care);
+    List<CropControl> findByDisease(Disease disease);
+    List<CropControl> findByPest(Pest pest);
 }
--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/crops/infrastructure/persistence/jpa/repositories/CropRepository.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/crops/infrastructure/persistence/jpa/repositories/CropRepository.java
@@ -1,4 +1,8 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.infrastructure.persistence.jpa.repositories;
 
-public interface CropRepository {
+import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.crops.domain.model.aggregates.Crop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CropRepository extends JpaRepository<Crop, Long>{
+
 }
