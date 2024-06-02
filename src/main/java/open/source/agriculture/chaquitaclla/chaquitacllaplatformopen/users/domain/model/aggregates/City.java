5d2f96511e8b2379c2519c0cba675137a294661b
Godofredo <u202120772@upc.edu.pe>
2024-06-02 11:02:47 -0500

    Gaa
    Xd

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/City.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/aggregates/City.java
@@ -12,9 +12,26 @@ public class City extends AuditableModel {
     private Long id;
     @Column(name = "name",nullable = false)
     private String name;
-    //@ManyToOne
-   // @JoinColumn(name = "country_id", nullable = false)
-   // private Country country;
-    
-
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Country getCountry() {
+        return country;
+    }
+    public void setCountry(Country country) {
+        this.country = country;
+    }
 }
--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/entities/Country.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/entities/Country.java
@@ -1,4 +1,25 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name",nullable = false)
+    private String name;
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
