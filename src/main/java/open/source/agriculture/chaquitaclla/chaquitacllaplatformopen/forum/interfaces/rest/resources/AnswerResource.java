2decb8192dac338869091514d735e6fed0f30ddd
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-06-03 18:17:35 -0500

    feat(forum): Corrected imports.

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/interfaces/rest/resources/AnswerResource.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/forum/interfaces/rest/resources/AnswerResource.java
@@ -1,6 +1,4 @@
 package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.interfaces.rest.resources;
 
-import open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.forum.domain.model.aggregates.Question;
-
 public record AnswerResource(Long id, Long userId, Long questionId, String answer) {
 }
