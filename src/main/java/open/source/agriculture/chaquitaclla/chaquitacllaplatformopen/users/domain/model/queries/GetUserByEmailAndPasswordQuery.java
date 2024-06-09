4d31ab42d5f41ca50f60de1c5ceb8c9db7ca7a55
Godofredo <u202120772@upc.edu.pe>
2024-06-09 09:42:23 -0500

    ComQue
    ...

---
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/users/domain/model/queries/GetUserByEmailAndPasswordQuery.java
@@ -0,0 +1,7 @@
+package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen.users.domain.model.queries;
+
+public record GetUserByEmailAndPasswordQuery(String email,
+                                             String password)
+{
+
+}
