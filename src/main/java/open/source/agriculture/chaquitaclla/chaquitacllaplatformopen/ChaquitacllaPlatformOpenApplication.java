8623fe31b9c630cbc7624bfb3b506a303517e7d3
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-06-10 09:58:16 -0500

    feat(forum): added open api.

--- src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/ChaquitacllaPlatformOpenApplication.java
+++ src/main/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/ChaquitacllaPlatformOpenApplication.java
@@ -2,7 +2,9 @@ package open.source.agriculture.chaquitaclla.chaquitacllaplatformopen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChaquitacllaPlatformOpenApplication {
 
